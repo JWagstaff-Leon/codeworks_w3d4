10811c41c9a57d499905937716f1e0e4631902ef
Joshua Wagstaff-Leon <wagstaffleon.joshua@gmail.com>
2022-04-07 15:16:31 -0600

    added dummy trips

--- app/AppState.js
+++ app/AppState.js
@@ -3,9 +3,15 @@ import { isValidProp } from "./Utils/isValidProp.js"
 import { Trip } from "./Models/Trip.js"
 import { Reservation } from "./Models/Reservation.js"
 
+const trip1 = new Trip({name: "trip1", description: "this is a trip"});
+const trip2 = new Trip({name: "trip2", description: "this is a trip"});
+const trip3 = new Trip({name: "trip3", description: "this is a trip"});
+const trip4 = new Trip({name: "trip4", description: "this is a trip"});
+const trip5 = new Trip({name: "trip5", description: "this is a trip"});
+
 class AppState extends EventEmitter {
   /**@type {import('./Models/Trip').Trip[]} */
-  trips = []
+  trips = [trip1, trip2, trip3, trip4, trip5]
 
   /**@type {import('./Models/Reservation').Reservation[]} */
   reservations = []
