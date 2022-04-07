2889c39ab261e7f0eb8780dbe310621a9af5caa6
EricMGustafson <emgustafson22@gmail.com>
2022-04-07 16:32:54 -0600

    stuff

--- app/Services/TripsService.js
+++ app/Services/TripsService.js
@@ -14,6 +14,7 @@ class TripsService
         const newTrip = new Trip(tripData);
         ProxyState.trips = [...ProxyState.trips, newTrip];
         ProxyState.currentTrip = newTrip.id;
+        console.log(ProxyState.trips);
     }
 
     deleteTrip(id)
